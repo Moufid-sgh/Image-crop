79bfc073074cccdbf952a4d7db7cdffa32fab9e4
Moufid <moufid.sgh@protonmail.com>
2023-08-03 11:37:49 +0100

    set-up

---
+++ image-crop/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       
+      
+      <p className="text-2xl text-red-200">
+        hello
+      </p>
+    </>
+  )
+}
+
+export default App
